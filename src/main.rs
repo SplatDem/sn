794761a3b07c00f670e6c84f33eacb8ba47921b8
SplatDem <egor2009212@gmail.com>
2024-11-17 21:00:23 +0300

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,130 @@
+use clap::Parser;
+use std::{
+    env,
+    fs::{File, OpenOptions},
+    io::{BufRead, BufReader, Error, Write},
+    path::Path,
+};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    notify: Option<String>,
+
+    #[arg(short, long)]
+    date: Option<String>,
+
+    #[arg(short, long)]
+    clear: bool,
+
+    #[arg(short, long)]
+    important: Option<u8>,
+}
+
+fn main() -> Result<(), Error> {
+    #[allow(deprecated)] // STUPID "WINDOWS WARNING" I HATE IT!!!!!
+    let home_dir_path = match env::home_dir() {
+        Some(path) => path.to_string_lossy().into_owned(),
+        None => "/".to_string(),
+    };
+
+    let data_path_format = format!("{}/.config/sn.txt", home_dir_path);
+    let data_path = Path::new(&data_path_format);
+    let data = data_path.display().to_string();
+
+    create_file(&data);
+
+    match get_args(&data) {
+        Ok(()) => print!(""),
+        Err(_) => print!(""),
+    }
+    Ok(())
+}
+
+fn get_args(data: &str) -> Result<(), Error> {
+    let args = Args::parse();
+
+    match (args.notify, args.date, args.important) {
+        (Some(notify), Some(date), None) => {
+            let mut file = OpenOptions::new()
+                .write(true)
+                .create(true)
+                .append(true)
+                .open(data)?;
+
+            writeln!(file, "{} -- {}", notify, date)?;
+        }
+        (Some(notify), None, None) => {
+            let mut file = OpenOptions::new()
+                .write(true)
+                .create(true)
+                .append(true)
+                .open(data)?;
+
+            writeln!(file, "{}", notify)?;
+        }
+        (None, Some(date), None) => {
+            let mut file = OpenOptions::new()
+                .write(true)
+                .create(true)
+                .append(true)
+                .open(data)?;
+
+            writeln!(file, "{}", date)?;
+        }
+        (None, None, None) => {
+            let to_read = File::open(data)?;
+            let read = BufReader::new(to_read);
+
+            for line in read.lines() {
+                println!("{}", line?);
+            }
+        }
+        (Some(notify), None, Some(important)) => {
+            let mut file = OpenOptions::new()
+                .write(true)
+                .create(true)
+                .append(true)
+                .open(data)?;
+
+            writeln!(file, "<== {} | {} ==>", important, notify)?;
+        }
+        (Some(notify), Some(date), Some(important)) => {
+            let mut file = OpenOptions::new()
+                .write(true)
+                .create(true)
+                .append(true)
+                .open(data)?;
+
+            writeln!(file, "<== {} -- {} | {} ==>", date, notify, important)?;
+        }
+        _ => todo!(),
+    }
+
+    if args.clear {
+        match clear_file(data) {
+            Ok(data) => data,
+            Err(e) => println!("{e}"),
+        }
+    }
+    Ok(())
+}
+
+fn create_file(data: &str) {
+    if !std::path::Path::new(data).exists() {
+        match File::create(data) {
+            Ok(_) => println!("{} was created", data),
+            Err(_) => println!("Failed to create file"),
+        }
+    } else {
+        print!("");
+    }
+}
+
+fn clear_file(data: &str) -> Result<(), Error> {
+    let _file = OpenOptions::new().write(true).truncate(true).open(data)?;
+
+    println!("==Cleared==");
+    Ok(())
+}
